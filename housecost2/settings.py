8108333d28fa9be576661985d29a61ed9666d02f
petershashya <petershashya01@gmail.com>
2026-05-29 18:50:54 +0300

    latest changes for buildcostapp

--- housecost2/settings.py
+++ housecost2/settings.py
@@ -168,11 +168,6 @@ USE_TZ = True
 
 #For hosting able static files
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
